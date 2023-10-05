9a6a2d545c56438229eac5b0491ffe910e364805
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-05 19:59:06 +0900

    프로젝트에 Spring Data Jpa 적용시키고 테스트하기

---
+++ src/main/java/com/learn/awsspringlearn/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.learn.awsspringlearn.domain.posts;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor  // 기본 생성자 자동 추가. public Posts() {}
+@Entity             // 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+    @Id     // PK 필드임을 나타냅니다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙을 나타내는 것으로 IDENTITY는 auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)     // 테이블의 칼럼을 나타내며 굳이 선언하지 않아도 되지만 값 변경이 필요할 때 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder        // 해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/learn/awsspringlearn/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.learn.awsspringlearn.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach // JUnit5 <-> @After JUnit4, Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정. 배포 전 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사용
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 불러오기")
+    public void postSaveLoad() {
+        // given
+        String title = "테스트 게시물";
+        String content = "테스트 본문";
+
+        postsRepository.save(       // insert/update 쿼리를 실행, id값이 있다면 update가 없다면 insert 쿼리가 실행
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("testauthor12@gmail.com")
+                        .build());
+        // when
+        List<Posts> list = postsRepository.findAll();   // 테이블 post에 있는 모든 데이터 조회
+
+        // then
+        Posts post = list.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getContent()).isEqualTo(content);
+    }
+}
