77339ab671ab3cb3e500696991cbed0b6585583c
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-05 12:02:56 +0900

    Initial commit

---
+++ src/main/java/com/learn/awsspringlearn/AwsSpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.awsspringlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/awsspringlearn/AwsSpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.awsspringlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
