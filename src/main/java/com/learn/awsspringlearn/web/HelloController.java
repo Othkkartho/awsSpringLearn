7c72f92734f9b56058748c5fef4e50e4bcba2bba
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-05 19:26:28 +0900

    lombok 설치 후 dto 생성 및 테스트

--- src/main/java/com/learn/awsspringlearn/web/HelloController.java
+++ src/main/java/com/learn/awsspringlearn/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.learn.awsspringlearn.web;
 
+import com.learn.awsspringlearn.web.dto.HelloResponseDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON을 반환(return)하는 컨트롤러로 만들어 줌
@@ -12,5 +14,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // @RequestParam: 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    // 여기에선 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메서드 파라미터 name(String name)에 저장
+    @GetMapping("/hello/dto")
+    public HelloResponseDTO helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDTO(name, amount);
     }
 }
--- src/test/java/com/learn/awsspringlearn/web/HelloControllerTest.java
+++ src/test/java/com/learn/awsspringlearn/web/HelloControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // @SpringBootTest를 적용하면 @ExtendWith(SpringExtension.class)를 포함하고 있어 생략이 가능
 @ExtendWith(SpringExtension.class)                  // JUnit5에서 이전 @RunWith(SpringRunner.class)을 대체
@@ -20,13 +21,34 @@ public class HelloControllerTest {
     @Autowired              // 스프링 빈(Bean)을 주입
     private MockMvc mvc;    // 개발한 웹 프로그램을 실제 서버에 배포하지 않고도 테스트를 위한 요청을 제공 즉, 웹 API 테스트 할 때 사용, 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트
 
-    @Test                                               // test할 메서드
+    @Test                                                   // test할 메서드
     @DisplayName("hello가 리턴된다")
-    public void returnHello() throws Exception {
+    public void returnHello() {
         String hello = "hello";
-        mvc.perform(get("/hello"))            // MockMvc를 통해 /hello 주소로 HTTP GET 요청, 체이닝이 지원되어 아래와 같이 여러 검증 기능을 이어서 선언
-                .andExpect(status().isOk())             // mvc.perform의 결과를 검증, HTTP Header의 Status를 검증, 우리가 흔히 알고 있는 200, 404, 500등의 상태를 검증
-                .andExpect(content().string(hello));    // mvc.perform의 결과를 검증, 응답 본문의 내용을 검증, Controller에서 "hello"를 return 하기 때문에 이 값이 맞는지 검증
+        try {
+            mvc.perform(get("/hello"))            // MockMvc를 통해 /hello 주소로 HTTP GET 요청, 체이닝이 지원되어 아래와 같이 여러 검증 기능을 이어서 선언
+                    .andExpect(status().isOk())             // mvc.perform의 결과를 검증, HTTP Header의 Status를 검증, 우리가 흔히 알고 있는 200, 404, 500등의 상태를 검증
+                    .andExpect(content().string(hello));    // mvc.perform의 결과를 검증, 응답 본문의 내용을 검증, Controller에서 "hello"를 return 하기 때문에 이 값이 맞는지 검증
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Test
+    public void returnHelloDto() {
+        String name = "hello";
+        int amount = 1000;
+
+        try {
+            mvc.perform(
+                    get("/hello/dto")                               // HelloController의 GetMapping("/hello/dto")의 메핑 테스트 url 설정
+                            .param("name", name)                        // param(): API 테스트할 때 사용될 요청 파라미터를 설정. 단, 값은 String만 허용. 그래서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능
+                            .param("amount", String.valueOf(amount)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", is(name)))        // jsonPath(): JSON 응답값을 필드별로 검증할 수 있는 메서드. $을 기준으로 필드명을 명시
+                    .andExpect(jsonPath("$.amount", is(amount)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
