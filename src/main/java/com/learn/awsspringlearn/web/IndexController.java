d0ecb1c31a4208f56b09ddb668dab9de8e98737e
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-06 21:42:18 +0900

    게시글 수정/삭제 구현

--- src/main/java/com/learn/awsspringlearn/web/IndexController.java
+++ src/main/java/com/learn/awsspringlearn/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.learn.awsspringlearn.web;
 
 import com.learn.awsspringlearn.service.posts.PostsService;
+import com.learn.awsspringlearn.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,5 +17,12 @@ public class IndexController {
     public String index(Model model) {  // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. postsService.findAllDesc()에서 가져온 결과를 "posts"로 index.html에 전달.
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "postsUpdate";
     }
 }
